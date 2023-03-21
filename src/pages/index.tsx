054d6fbb66b017e32177cc1cb266aadcbdd4a6bf
Thomas Alday-Conrod <thomasconrod@gmail.com>
2023-03-20 18:41:25 -0700

    autocomplete search bar on front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import { Button, Container, Heading } from '@chakra-ui/react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { NextSeo } from 'next-seo'
+import { useEffect, useRef } from 'react'
 import { MdMyLocation } from 'react-icons/md'
+import { text } from 'stream/consumers'
 
 export default function Home() {
   const router = useRouter()
-
+  const inputRef = useRef<HTMLInputElement>(null)
   /** Get the user's location and redirect them to the map. */
   function getCurrentLocation() {
     if (!navigator.geolocation) {
@@ -20,6 +22,36 @@ export default function Home() {
       })
     }
   }
+  useEffect(() => {
+    // Load Google Places API script
+    const script = document.createElement('script')
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`
+    script.async = true
+    script.onload = () => {
+      // Initialize Google Places Autocomplete
+      const autocomplete = new window.google.maps.places.Autocomplete(inputRef.current as HTMLInputElement)
+      // Set the types of predictions to return
+      autocomplete.setTypes(['geocode'])
+      // Bias predictions to the user's current location
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const geolocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          }
+          const circle = new window.google.maps.Circle({
+            center: geolocation,
+            radius: position.coords.accuracy,
+          })
+          const bounds = circle.getBounds()
+          if (bounds) {
+            autocomplete.setBounds(bounds)
+          }
+        })
+      }
+    }
+    document.head.appendChild(script)
+  }, [])
 
   return (
     <>
@@ -39,9 +71,10 @@ export default function Home() {
             Map
           </Button>
         </Link>
-
         <br />
-
+        <Container p={5}>
+          <input id="autocomplete" placeholder="Enter a place" type="text" ref={inputRef}></input>
+        </Container>
         <Button leftIcon={<MdMyLocation />} colorScheme="facebook" mt={5} onClick={getCurrentLocation}>
           Use current location
         </Button>
