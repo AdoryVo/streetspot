d44c8b20875d96815766cd756a91372a7d9d0e3f
Thomas Alday-Conrod <thomasconrod@gmail.com>
2023-03-30 10:33:54 -0700

    Change Ionicon import

--- src/components/Report.tsx
+++ src/components/Report.tsx
@@ -8,7 +8,7 @@ import ky from 'ky'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
-import { IoLogoInstagram } from 'react-icons/Io'
+import { IoLogoInstagram } from 'react-icons/io'
 import {
   MdPlace, MdShare, MdThumbDown, MdThumbUp
 } from 'react-icons/md'
